0236a37a843c29ca97eae17bbd2769dadeeae250
AislynnEdmiston <davidandaislynn@gmail.com>
2019-04-20 14:03:08 -0400

    Completed the onClick function in tab.js

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,13 +5,16 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      const tabClass = props.selectedTab === props.tab ? 'tab active-tab' : 'tab';
+      const tabHandler = () => {
+        props.selectedTabHandler(props.tab);
+      }
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={tabClass}
+      onClick={tabHandler}
+
+
     >
       {props.tab.toUpperCase()}
     </div>
